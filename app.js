a35610b60954365b8f60fbdea20e66cec2228584
josMuiruri <josemuiruri51@gmail.com>
2026-03-11 09:26:15 +0300

    handling Patch request

--- app.js
+++ app.js
@@ -58,6 +58,22 @@ app.post('/api/v1/users', (req, res) => {
     );
 });
 
+app.patch('/api/v1/users/:id', (req, res) => {
+    if (req.params.id * 1 > users.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    })
+})
+
 const port = 3000;
 
 app.listen(port, () => {
