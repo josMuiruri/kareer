202499fd1568ac7d5049796535c7d6b94c9e2811
josMuiruri <josemuiruri51@gmail.com>
2026-03-24 12:50:20 +0300

    file structuring

--- app.js
+++ app.js
@@ -1,107 +1,18 @@
 const fs = require('fs');
 const express = require('express');
-const { get } = require('http');
+
+const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
+// middleware
+app.use(mogran('dev'));
 app.use(express.json());
 
-const users = fs.readFileSync();
 
-const getAllUsers = (req, res) => {
-    res.status(200).json({
-        status: 'sucess',
-        results: users.length,
-        data: {
-            users
-        }
-    });
-}
 
-const getUser = (req, res) => {
-    console.log(req.params);
+app.use('/api/v1/users', userRouter);
 
-    const id = req.params.id * 1;
-    const user = users.find(el => el.id === id);
-
-    if (!user) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user
-        }
-    })
-};
-
-const createUser = (req, res) => {
-    const newId = users[users.length - 1].id + 1;
-    const newUser = Object.assign({ id: newId }, req.body);
-
-    users.push(newUser);
-
-    fs.writeFile(
-        `${__dirname}/dev-data/data/users-simple.json`,
-        JSON.stringify(users),
-        err => {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    user: newUser
-                }
-            });
-        }
-    );
-};
-
-const updateUser = (req, res) => {
-    if (req.params.id * 1 > users.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            user
-        }
-    })
-};
-
-const deleteUser = (req, res) => {
-    if (req.params.id * 1 > users.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
-};
-
-app
-.route('/api/v1/users')
-.get(getAllUsers)
-.post(createUser);
-
-app
-.route('/api/v1/users/:id')
-.get(getUser)
-.patch(updateUser)
-.delete(deleteUser)
-
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-});
+module.exports = app;
